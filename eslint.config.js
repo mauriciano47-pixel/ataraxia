42b0a68263d6934ac27d998c3d1ffd35994e52ac
Mauricio <mauro@example.com>
2026-09-05 17:45:59 -0400

    fix(compiler): cerrar React.memo en GlowArcGauge y configurar flat eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,13 @@ const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
   {
     ignores: ["dist/*"],
     rules: {
       "react-hooks/refs": "off",
       "react-hooks/set-state-in-effect": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/purity": "off",
       "react/no-unescaped-entities": "off",
       "react-hooks/exhaustive-deps": "warn",
       "import/first": "warn"
